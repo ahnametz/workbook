796c7736d7b9c98b6d3da79a5d091c01ddeab703
Don Welch <dwelch@nait.ca>
2023-03-06 18:45:50 -0700

    Create Playlist web application #6

---
+++ PlaylistManagement/WebApp/Pages/Samples/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Samples
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
